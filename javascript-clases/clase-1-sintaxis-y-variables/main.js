a2c7f12ba88f03b1d451dd6c02c161ceb6f4d395
RojasRayner <107149847+RojasRayner@users.noreply.github.com>
2022-08-21 16:56:40 -0300

    Primer actividad en clase

---
+++ javascript-clases/clase-1-sintaxis-y-variables/main.js
@@ -0,0 +1,18 @@
+//ACTIVIDADES EN CLASE
+//Crear un algoritmo simple.
+//pedimos Un nombre y un numero y dos textos
+//al nombre debemos concatenarlo con la frase: Bienvenido + nombre
+//al numero debemos parsearlo y sumarlo con otro valor almacenado previamente y mostrarlo en consola
+//concatenar los dos textos.
+const year = 2022;
+const bienvenida = "Eres Bienvenido aqui ";
+let nombre = prompt ("Ingrese su Nombre: ");
+let nacimiento = prompt ("Ingrese año de nacimiento: ");
+let textoUno = prompt ("Que estudia en Coderhouse?");
+let textDos = prompt ("Cual es su meta al estudiar " + textoUno);
+//mostrarlo los resultados.
+alert(bienvenida+nombre);
+Number(edad = year - nacimiento);
+console.log("Tiene "+edad+" de edad");
+alert(`Estudias en Coderhouse ${textoUno}\nTu deseo es\n${textDos}`);
+alert("Tienes de edad " + edad);
