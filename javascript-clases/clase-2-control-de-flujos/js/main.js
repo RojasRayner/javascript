193ba18f8ca2f5ca03aabb111d8b4ddfc4d60b28
RojasRayner <107149847+RojasRayner@users.noreply.github.com>
2022-08-23 20:59:21 -0300

    Detalles del Login

--- javascript-clases/clase-2-control-de-flujos/js/main.js
+++ javascript-clases/clase-2-control-de-flujos/js/main.js
@@ -1,36 +1,61 @@
 //Actividad 2 Crear un algoritmo con un condicional
-//solicitar uno o mas valores ingresados pro prompt
+//solicitar uno o mas valores ingresados por prompt
 //comparar las entradas en funcion a ciertas condiciones
 //muestre por consola o alert el resultado segun los valores ingresados y las condiciones cumplidas
 
 
 //variable string que se usa como guia para comparar
-let palabra = ("hola");
-//variable que se usa para guardar la string que se va a generar de las letras en el nombre
-let saludo;
-//se pide datos al usuario
-let nombre = prompt ("Ingrese su nombre: ");
-//un loop para comparar que el datos del usuario no tenga numeros
-for (let i = 0; i < nombre.length; i++) {
+let palabra = (`@/*-|"\\_`);
+//numeros de intentos
+let intentos = 3;
+//se pide datos al usuario y se les espesifica las validaciones
+let nombre = prompt (`\t Tienes ${intentos} intentos \n\t Ingrese su nombre \n\t No usar las siguientes Caracteres Restringidos\n @ / * - | " \\ _ `);
+//mientras el valor no sea escape y tenga cantidad de intentos, ejecutar.
+while ((nombre !== "ESC") && (intentos > 1)) {
+    //variable que se usa para guardar la string que se va a generar de las letras en el nombre
+    let restringida = "";
+    //variable que se usa para guardar los numeros ingresados por el usuario
+    let numero = "";
+    //un loop para comparar que el datos del usuario no tenga numeros
+    for (let i = 0; i < nombre.length; i++) {
     const element = nombre[i];
-    //si el dato del usuario tiene un numero generar un alert
-    if(element >=0){
-        alert(`Ingresaste este numero ${element} \nporfavor ingresa correctamente tu nombre`);
-    }else{
-        //de lo contrario comparar el datos del usuario con la variable que se usa como guia
-        for (let i = 0; i < nombre.length; i++) {
-            for (let index = 0; index < palabra.length; index++) {
-                const elemento = palabra[index];
-                if(nombre[i] === elemento){
-                    saludo[index] = nombre[i];
-                }else{
-                    continue;
-                }
+    //si el dato del usuario tiene un numero guardar ese numero
+        if(Number(element) >=0){
+            numero = numero + element.toString();
+        }else{
+            continue;
+        }
+    }
+    //de lo contrario comparar el datos del usuario con la variable que se usa como palabra reservada
+    for (let i = 0; i < nombre.length; i++) {
+        for (let index = 0; index < palabra.length; index++) {
+            const elemento = palabra[index];
+            if(nombre[i] === elemento){
+                restringida= restringida + nombre[i];
+            }else{
+            continue;
             }
         }
-        alert(`se comparo y se encontro esta similitud\n${saludo}`);
+    }
+    //validaciones
+    //si numero contiene algun dato or restringida contiene algunn dato
+    if ((numero !== "") || (restringida !== "")) {
+        alert(`ingresaste por error:\n Numeros ${numero} \nPalabra Restringida ${restringida}`);
+    }else //si no hay ningun dato entonces
+        if((numero === "") && (nombre === "") && (restringida === "")){
+        alert("Incorrecto!. No ingresaste ningun dato."); 
+    }else{//de lo contrario
+        alert(`Bienvenido. ${nombre} Ingresaste correctamente`);
         break;
     }
-    break;
+    //se resta un intento
+    intentos--;
+    //se pide datos al usuario nuevamente
+    nombre = prompt (`\t Tienes ${intentos} intentos \n\tIngrese su nombre \n\t No usar las siguientes Caracteres Restringidos\n @ / * - | " \\ _ `);
+    if(intentos == 1){
+        alert(`Lo Sentimos!. No tienes más intentos`)
+        break;
+    }else{
+        continue;
+    }
 }
-alert(`${saludo} ingresaste correctamente tu nombre`);
