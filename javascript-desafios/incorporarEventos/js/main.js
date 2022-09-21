e494cb98adcc147d9f88b11342feed137c3aa693
RojasRayner <107149847+RojasRayner@users.noreply.github.com>
2022-09-21 20:34:53 -0300

    incorporando eventos 1.0

--- javascript-desafios/incorporarEventos/js/main.js
+++ javascript-desafios/incorporarEventos/js/main.js
@@ -1,13 +1,26 @@
 //INCORPORAR EVENTOS
+//PASOS A SEGUIR EN EL PROYECTO MIABONITA ESTETICA
+
+//1.- DEBE ESCOGER ENTRE LOS DIFERENTES SERVICIOS O PRODUCTOS
+//2.- SI ES SERVICIOS ESCOGER: A.- EL SERVICIO B.- LA FECHA Y HORA
+//3.- SI ES PRODUCTOS ESCOGER: C.- EL PRODUCTO D.- LA CANTIDAD
+//4.- SUMAMOS SERVICIO + PRODUCTOS: E.- LA FORMA DE PAGO F.- RESUMEN DE COMPRA EN FORMA DE CARRITO DE COMPRA.
+//5.- EL USUARIO DEBE INGRESAR EN UNA CUENTA
+
+//NECESITAMOS LAS SIGUIENTES VARIABLES y OBJETOS
+//1.- USUARIOS
+//2.- SERVICIOS
+//3.- PRODUCTOS
+//4.- CARRITO
+//5.- CONFIGURACION USUARIO
 
 
-//VARIABLES GLOBALES
 const administradores = [{superUsuario:"admin",password:"admin"}];
-const usuarios = [];
 const clientes = [];
-const servicios = [{titulo:"Limieza Facial",des:"",precio:300,horaTrata:1},{titulo:"Maniqura",des:"",precio:600,horaTrata:2},{titulo:"Pediqura",des:"",precio:600,horaTrata:2}];
-const productos =[{titulo:"AGUA MICELAR",id: 1,precio:300,tipoDePiel:"NORMAL",cant:10},{titulo:"JABON FACIAL",id: 2,precio:120,tipoDePiel:"NORMAL",cant:10},{titulo:"ESPUMA DESMAQUILLANTE",id: 3,precio:450,tipoDePiel:"MIXTA",cant:10},{titulo:"TONICO HIDRATANTE",id: 4,precio:210,tipoDePiel:"SECA",cant:10},{titulo:"TONICO REVITALIZANTE",id: 5,precio:210,tipoDePiel:"GRASA",cant:10}];
+const servicios = [{titulo:"Limieza Facial",des:"Procedimiento que elimina todas las impurezas que se encuentran en la piel generadas por el mismo cuerpo y por el ambiente.",precio:300,horaTrata:1,img:"./images/productos/limpiezaFacial.png",id:1},{titulo:"Maniqura",des:" Tratamiento de belleza cosmético para las uñas y manos",precio:600,horaTrata:2,img:"./images/productos/maniqura.png",id:2},{titulo:"Pediqura",des:"Tratamiento de belleza cosmético para las uñas y pies.",precio:600,horaTrata:2,img:"./images/productos/pediqura.png",id:3}];
+const productos =[{titulo:"AGUA MICELAR",id:1,precio:300,tipoDePiel:"NORMAL",cant:10,img:"./images/productos/aguaMicelar.png"},{titulo:"JABON FACIAL",id:2,precio:120,tipoDePiel:"NORMAL",cant:10,img:"./images/productos/jabonFacial.png"},{titulo:"ESPUMA DESMAQUILLANTE",id:3,precio:450,tipoDePiel:"MIXTA",cant:10,img:"./images/productos/espumaDesmaquillante.png"},{titulo:"TONICO HIDRATANTE",id:4,precio:210,tipoDePiel:"SECA",cant:10,img:"./images/productos/tonicoHidratante.png"},{titulo:"TONICO REVITALIZANTE",id:5,precio:210,tipoDePiel:"GRASA",cant:10,img:"./images/productos/tonicoRevitalizante.png"}];
 const horaDeTrabajo = [];
+const agendaServicios = [];
 function FechaServicios(fecha,hora,servicioUsuario,horaTrata,nombreUsuario,telUsuario,passUsuario){
     this.cliente = nombreUsuario;
     this.telefono = telUsuario;
@@ -18,119 +31,109 @@ function FechaServicios(fecha,hora,servicioUsuario,horaTrata,nombreUsuario,telUs
     this.password = passUsuario
 }
 const carritoCompra = [];
-let nombreUsuario;
-let passUsuario;
-
-//CAPTURAMOS LOS ELEMENTOS DE LA PAGINA DE INICIO
-//COMIENZO DEL SIMULADOR
 
 let main = document.querySelector("main");
+let header = document.querySelector("header");
+let imgCarrito = header.querySelector("i");
+imgCarrito.addEventListener("click",verCarrito);
+
+let paginaCarrito = document.querySelector("#paginaCarrito");
+let templateCarrito = paginaCarrito.content.querySelector(".wrapper");
+let carritoInner = templateCarrito.querySelector(".carrito");
+carritoInner.style.background = "none";
+
+function verCarrito(e){
+    
+    templateIndex.remove();
+    main.appendChild(templateCarrito);
+    let cardCarritoInnerIndex = carritoInner.querySelector(".cardCarrito");
+    cardCarritoInnerIndex.remove();
+    
+    if((carritoCompra.length == 0) && (agendaServicios.length == 0)){
+        let agregar = document.createElement("h2");
+        agregar.innerHTML = "<h2>NO TIENES PRODUCTOS NI SERVICIOS EN TU CARRITO</h2>";
+        templateCarrito.appendChild(agregar);
+    }else{
+        let totalCompra = 0;
+        let spanCarritoInnerIndex = carritoInner.querySelector("span");
+        spanCarritoInnerIndex.textContent = `TOTAL DE COMPRA: ${totalCompra}`;
+        carritoCompra.forEach(element => {
+            let cardCarritoInnerIndexClonada = cardCarritoInnerIndex.cloneNode(true);
+            let imgCardCarritoInnerIndexClonada = cardCarritoInnerIndexClonada.querySelector("img");
+            imgCardCarritoInnerIndexClonada.setAttribute("src",`${element.img}`);
+            let parrfCardCarritoInnerIndexClonada = cardCarritoInnerIndexClonada.querySelector("p");
+            parrfCardCarritoInnerIndexClonada.textContent = `Para Piel: ${element.tipoDePiel}\tPrecio: ${element.precio}$`;
+            let labelCardCarritoInnerIndexClonada = cardCarritoInnerIndexClonada.querySelector("label");
+            labelCardCarritoInnerIndexClonada.textContent = `${element.titulo}`;
+            let inputCardCarritoInnerIndexClonada = cardCarritoInnerIndexClonada.querySelector("input");
+            inputCardCarritoInnerIndexClonada.setAttribute("max",`${element.cant}`);
+            inputCardCarritoInnerIndexClonada.setAttribute("disabled",true);
+            totalCompra = totalCompra + (element.precio * 1);
+            let buttonCardCarritoInnerIndexClonada = cardCarritoInnerIndexClonada.querySelector("button");
+            buttonCardCarritoInnerIndexClonada.addEventListener("click", eliminarProducto);
+            buttonCardCarritoInnerIndexClonada.setAttribute("id",`${element.id}`);
+            carritoInner.appendChild(cardCarritoInnerIndexClonada);
+        });
+        
+    }
+}
+//funcion que elimina productos del carrito de compra
+function eliminarProducto(e){
+    let eliminarProducto = carritoCompra.indexOf(e.target.id);
+    carritoCompra.splice(eliminarProducto,1);
+    verCarrito();
+}
+
+
+
 
 let paginaIndex = document.querySelector("#paginaIndex");
 let templateIndex = paginaIndex.content.querySelector(".wrapper");
 main.appendChild(templateIndex);
 
-let inputTextNomUsuario = templateIndex.querySelector(".nombreUsuario");
-inputTextNomUsuario.setAttribute("required",true);
+let productosInner = templateIndex.querySelector(".productos");
+productosInner.style.background = "none";
+let cardProductosInnerIndex = productosInner.querySelector(".cardProductos");
+cardProductosInnerIndex.remove();
 
-inputTextNomUsuario.addEventListener("input", (e)=>{
-    nombreUsuario = e.target.value;
-    usuarios.push(nombreUsuario);
-    return usuarios;
+//MOSTRAR POR CADA PRODUCTO UNA CARD CON SUS ESPESIFICACIONES
+productos.forEach(element => {
+    let cardProductosInnerIndexClonada = cardProductosInnerIndex.cloneNode(true);
+    let parrfCardProductosInnerIndexClonada = cardProductosInnerIndexClonada.querySelector("p");
+    let imgCardProductosInnerIndexClonada = cardProductosInnerIndexClonada.querySelector("img");
+    imgCardProductosInnerIndexClonada.setAttribute("src",`${element.img}`);
+    parrfCardProductosInnerIndexClonada.textContent = `${element.titulo}.\n\nTIPO DE PIEL: ${element.tipoDePiel}\nPRECIO: ${element.precio}$\nCANTIDAD EN EXISTENCIA: ${element.cant}`;
+    productosInner.appendChild(cardProductosInnerIndexClonada);
+    let productoId = element.id;
+    let botonComprar = cardProductosInnerIndexClonada.querySelector("button");
+    botonComprar.setAttribute("id",`${productoId}`);
+    botonComprar.addEventListener("click",agregarProductos);
 });
 
-let inputTextPassUsuario = templateIndex.querySelector(".passUsuario");
-inputTextPassUsuario.setAttribute("required",true);
-
-inputTextPassUsuario.addEventListener("input", (e)=>{
-    passUsuario = e.target.value;
-    usuarios.push(passUsuario);
-    return usuarios;
+let serviciosInner = templateIndex.querySelector(".servicios");
+serviciosInner.style.background = "none";
+let cardServiciosInnerIndex = serviciosInner.querySelector(".cardServicios");
+cardServiciosInnerIndex.remove();
+//POR CADA SERVICIO MUESTRE UNA CARD CON LAS ESPESIFICACIONES
+servicios.forEach(element => {
+    let cardServiciosInnerIndexClonada = cardServiciosInnerIndex.cloneNode(true);
+    let parrfCardServiciosInnerClonada = cardServiciosInnerIndexClonada.querySelector("p");
+    let imgCardServiciosInnerClonada = cardServiciosInnerIndexClonada.querySelector("img");
+    imgCardServiciosInnerClonada.setAttribute("src",`${element.img}`);
+    parrfCardServiciosInnerClonada.textContent = `${element.titulo}\n\nDESCRIPCION: ${element.des}\nHORA DE TRATAMIENTO: ${element.horaTrata} HORAS`;
+    serviciosInner.appendChild(cardServiciosInnerIndexClonada);
+    let servicioId = element.id;
+    let botonAgendar = cardServiciosInnerIndexClonada.querySelector("button");
+    botonAgendar.setAttribute("id",`${servicioId}`);
+    botonAgendar.addEventListener("click",agregarServicios);
 });
-
-let formularioUsuario = templateIndex.querySelector(".entrada");
-formularioUsuario.addEventListener("submit", loguin);
-
-console.log(usuarios);
-//FUNCION DE BUSQUEDA DE USUARIO
-function loguin(usuarios) {
-    console.log(usuarios)
-    for (const administrado of administradores) {
-        if((usuarios[0] == administrado.superUsuario) && (usuarios[1] == administrado.password)){
-            templateIndex.remove();
-            let templateAdministrador = document.createElement("div");
-            templateAdministrador.innerHTML = `
-            <div class="wrapper">
-                <div class="inner">
-                    <form action="">
-                        <div class="d-flex justify-content-center align-items-center form-wrapper flex-column">
-                            <h2>MENU DE ADMINISTRADOR</h2>
-                            <h1 class="usuario">USUARIO: ${usuarios[0]}</h1>
-                            <h3 class="infoMenu">INFORMACION PARA EL MENU</h3>
-                        </div>
-                        <div class="botonera">
-                            <a href="#" class="btn btn-primary productos mb-2 btn-lg">PRODUCTOS</a>
-                            <a href="#" class="btn btn-primary servicios mb-2 btn-lg">SERVICIOS</a>
-                            <a href="#" class="btn btn-primary administradores mb-2 btn-lg">ADMINISTRADORES</a>
-                            <a href="#" class="btn btn-primary salir mb-2 btn-lg">SALIR</a>
-                        </div>
-                    </form>
-                </div>
-            </div>`;
-            main.appendChild(templateAdministrador);
-            let botonera = templateAdministrador.querySelector(".botonera");
-            let botonProductos = botonera.querySelector(".productos");
-            let botonServicios = botonera.querySelector(".servicios");
-            let botonAdministradores = botonera.querySelector(".administradores");
-            let botonSalir = botonera.querySelector(".salir");
-
-            botonProductos.addEventListener("click",()=>{
-                manejarProductos();
-            });
-
-            botonServicios.addEventListener("click",()=>{
-                manejarServicios();
-            });
-
-            botonAdministradores.addEventListener("click",()=>{
-                manejarAdministradores();
-            });
-
-            botonSalir.addEventListener("click",()=>{
-                Salir();
-            });
-
-        }else if(clientes.length == 0) {
-            templateIndex.remove();
-            agregarCliente(usuarios);
-        }
-    }
+//funcion que agrega al carrito los productos que quiere comprar
+function agregarProductos(e){
+    let posibleCompra = productos.find(prod => prod.id == e.target.id);
+    carritoCompra.push(posibleCompra);
 }
-//FUNCION PARA AGREGAR CLIENTES
-function agregarCliente(usuarios){
-
-            let templateRegistroCliente = document.createElement("div");
-            templateRegistroCliente.innerHTML = `
-            <div class="wrapper">
-                <div class="inner">
-                    <form action="" class="registroNuevoUsuario">
-                        <h3>FORMULARIO DE REGISTRO NUEVO CLIENTE</h3>
-                        <div class="form-group">
-                            <div class="form-wrapper">
-                                <label for="nombreUsuario">Nombre de Usuario</label>
-                                <input type="text" class="form-control nombreUsuario" required placeholder="Ingrese nombre de Usuario" value=${usuarios[0]}>
-                            </div>
-                        </div>
-                        <div class="form-wrapper">
-                            <label for="passUsuarioDos">Password</label>
-                            <input type="password" class="form-control passUsuarioDos password" required placeholder="Ingrese Password" aria-label="password">
-                            <label for="passUsuarioTres">Password</label>
-                            <input type="password" class="form-control passUsuarioTres password" required placeholder="Ingrese Password" aria-label="password">
-                        </div>
-                        <button class="btn btn-primary entrarRegistrar mb-2 btn-lg" type="submit">Registrar</button>
-                        <button type="submit" class="btn btn-primary salir mb-2 btn-lg">SALIR</button>
-                    </form>
-                </div>
-            </div>`;
-            main.appendChild(templateRegistroCliente);
+//funcion que agrega a la agenda los servicios que quiere agendar
+function agregarServicios(e){
+    let agendaCliente = servicios.find( serv => serv.id == e.target.id);
+    agendaServicios.push(agendaCliente);
 }
